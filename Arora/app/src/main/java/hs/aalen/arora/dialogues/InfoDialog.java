94dd83a0936b42b91163beabac478845f97ad703
Michael Schlosser <michael.schlosser@studmail.htw-aalen.de>
2021-07-25 20:12:58 +0200

    Add Info Text Dialog to Main Activity

--- Arora/app/src/main/java/hs/aalen/arora/dialogues/InfoDialog.java
+++ Arora/app/src/main/java/hs/aalen/arora/dialogues/InfoDialog.java
@@ -2,22 +2,29 @@ package hs.aalen.arora.dialogues;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 import hs.aalen.arora.R;
 
 public class InfoDialog implements Dialog {
     private AlertDialog infoDialog;
+    private Context context;
 
     @Override
     public void createDialog(Context context) {
+        this.context = context;
         AlertDialog.Builder infoDialogBuilder = new AlertDialog.Builder(context);
-
-        final View infoDialogView = ((LayoutInflater)context
+        final View infoDialogView = ((LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
                 .inflate(R.layout.info_dialog_popup, null);
+
+        TextView infoText = infoDialogView.findViewById(R.id.info_text);
+        infoText.setText(Html.fromHtml(context.getString(R.string.info_text)));
+        infoText.setMovementMethod(LinkMovementMethod.getInstance());
         Button okButton = infoDialogView.findViewById(R.id.button_info);
 
         infoDialogBuilder.setView(infoDialogView);
