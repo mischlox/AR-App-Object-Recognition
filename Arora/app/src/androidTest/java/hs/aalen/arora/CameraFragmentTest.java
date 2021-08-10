b1734cacd243d70b2418b8097392299f95647bfc
Michael Schlosser <michael.schlosser@studmail.htw-aalen.de>
2021-08-10 23:46:53 +0200

    Continue writing tests chapter

---
+++ Arora/app/src/androidTest/java/hs/aalen/arora/CameraFragmentTest.java
@@ -0,0 +1,18 @@
+package hs.aalen.arora;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+public class CameraFragmentTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
